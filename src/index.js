3abd9c409e7ae1ee7f09653d6acf12d95cb836cf
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-02-20 02:50:47 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  var array = String(n).split('');
+  if (array[0]=='-'){
+     array.splice(0, 1);
+  }
+  let z = '';
+  for (let i=array.length-1; i>=0; i--) {
+      z=z+array[i];
+  }
+  let num = Number(z);
+    return(num);
 }
